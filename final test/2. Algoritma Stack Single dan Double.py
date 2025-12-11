6fe413dba076aa70892894476ac4726594554a7b
nopalsisfo2025 <nopalk6372@gmail.com>
2025-12-11 14:08:32 +0800

    Update 2. Algoritma Stack Single dan Double.py

--- final test/2. Algoritma Stack Single dan Double.py
+++ final test/2. Algoritma Stack Single dan Double.py
@@ -1,1 +1,15 @@
 
+size = 10
+stack = [None] * size
+top1 = -1
+top2 = size
+
+# Push ke stack 1
+top1 += 1
+stack[top1] = 100
+
+# Push ke stack 2
+top2 -= 1
+stack[top2] = 200
+
+print("Double Stack:", stack)
