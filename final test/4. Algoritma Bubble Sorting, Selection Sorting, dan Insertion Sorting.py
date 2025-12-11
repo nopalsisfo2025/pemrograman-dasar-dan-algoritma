24ea1a44490cb9e60ee877283aa5d00da6f06edb
nopalsisfo2025 <nopalk6372@gmail.com>
2025-12-11 14:11:51 +0800

    Update 4. Algoritma Bubble Sorting, Selection Sorting, dan Insertion Sorting.py

--- final test/4. Algoritma Bubble Sorting, Selection Sorting, dan Insertion Sorting.py
+++ final test/4. Algoritma Bubble Sorting, Selection Sorting, dan Insertion Sorting.py
@@ -1,1 +1,39 @@
+# bbule sort
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
+# select sort
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+
+# insert sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+
+        arr[j+1] = key
+
+    return arr
+
+print("Insertion Sort:", insertion_sort([5,3,8,4,2]))
+print("Bubble Sort:", bubble_sort([5,3,8,4,2]))
+print("Selection Sort:", selection_sort([5,3,8,4,2]))
