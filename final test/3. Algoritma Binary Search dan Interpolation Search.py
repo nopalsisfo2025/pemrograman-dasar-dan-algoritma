40598ede0b48f6b300fcc347b910c1e2a43cccf4
nopalsisfo2025 <nopalk6372@gmail.com>
2025-12-11 14:09:40 +0800

    Update 3. Algoritma Binary Search dan Interpolation Search.py

--- final test/3. Algoritma Binary Search dan Interpolation Search.py
+++ final test/3. Algoritma Binary Search dan Interpolation Search.py
@@ -1,1 +1,39 @@
 
+def binary_search(arr, x):
+    low, high = 0, len(arr)-1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+data = [10,20,30,40,50]
+print("Binary Search cari 30:", binary_search(data, 30))
+
+
+# interpolation search
+def interpolation_search(arr, x):
+    low, high = 0, len(arr)-1
+
+    while low <= high and x >= arr[low] and x <= arr[high]:
+        pos = low + ((x - arr[low]) * (high - low)) // (arr[high] - arr[low])
+
+        if arr[pos] == x:
+            return pos
+        elif arr[pos] < x:
+            low = pos + 1
+        else:
+            high = pos - 1
+
+    return -1
+
+data = [10,20,30,40,50]
+print("Interpolation Search cari 40:", interpolation_search(data, 40))
+
