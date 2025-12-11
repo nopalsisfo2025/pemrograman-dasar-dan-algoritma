cc5e91151fbecc8aa7b024d21c75bad0ec869324
nopalsisfo2025 <nopalk6372@gmail.com>
2025-12-11 14:12:27 +0800

    Update and rename 5. Dictionary to 5. Dictionary.py

---
+++ final test/5. Dictionary.py
@@ -0,0 +1,10 @@
+
+mahasiswa = {
+    "nama": "Budi",
+    "nim": "123456",
+    "jurusan": "Sistem Informasi",
+    "angkatan": 2023
+}
+
+print("Dictionary:", mahasiswa)
+print("Nama:", mahasiswa["nama"])
